482dc5ee25032dc8b50afbe5d3e555bc2400e005
Gading Nasution <account@gading.dev>
2025-02-09 12:32:10 +0700

    chore: refactor AudioPlayer

--- src/AudioPlayer.cpp
+++ src/AudioPlayer.cpp
@@ -1,13 +1,13 @@
 #include "AudioPlayer.h"
 
-Audio audio;
-
 AudioPlayer::AudioPlayer() {}
 
 void AudioPlayer::setup(SDCard* sdCard) {
   sd = sdCard;
   audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
-  audio.setVolume(18);
+  audio.setVolume(10);
+  audio.setTone(0,0,0);
+  audio.forceMono(true);
   Serial.println("AudioPlayer initialized");
 }
 
@@ -22,7 +22,20 @@ void AudioPlayer::play(const char* path) {
   Serial.print("Playing: ");
   Serial.println(fullPath);
 
-  audio.connecttoFS(sd->getSDMMC(), path);
+  // Add file information
+  // File file = sd->getSDMMC().open(path);
+  // if(file) {
+  //   Serial.print("File size: ");
+  //   Serial.print(file.size());
+  //   Serial.println(" bytes");
+  //   file.close();
+  // }
+
+  if (!audio.connecttoFS(sd->getSDMMC(), path)) {
+    Serial.println("Failed to connect to audio file");
+    return;
+  }
+  Serial.println("Successfully connected to audio file");
 }
 
 void AudioPlayer::loop() {
--- src/AudioPlayer.h
+++ src/AudioPlayer.h
@@ -5,17 +5,19 @@
 #ifndef AUDIOPLAYER_H
 #define AUDIOPLAYER_H
 
-#define I2S_BCLK 17
-#define I2S_LRC 18
-#define I2S_DOUT 16
+#define I2S_BCLK 17 // white wire
+#define I2S_LRC 18 // grey wire
+#define I2S_DOUT 16 // black wire
 
 class AudioPlayer {
 public:
   SDCard* sd;
+  Audio audio;
   AudioPlayer();
   void setup(SDCard* sdCard);
   void play(const char* path);
   void loop();
+  Audio getAudio() { return audio; };
 };
 
 #endif
